90db780206fc246f20b6bd6f41faa0685d31b2eb
michalnand <michal.nand@gmail.com>
2021-03-14 16:23:18 +0100

    rl + env for line follower

--- src/gym-line_follower/setup.py
+++ src/gym-line_follower/setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='gym_line_follower',
       version='1.0.0',
-      install_requires=['gym', 'numpy', 'pybullet']
+      install_requires=['gym', 'numpy', 'pybullet', 'shapely']
 )
